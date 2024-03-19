a72ffd81460e240d14626b19a825fbabb480897a
hyoje <hyoje@email.com>
2024-03-19 14:12:52 +0900

    update summing.py

--- summing.py
+++ summing.py
@@ -1,35 +1,50 @@
 import os
 import numpy as np
 
-def load_and_append_data(category, instance, elevation, azimuth, lightening):
-    """Load data from a file and append it to an existing array."""
-    file_path = f'./tmp/{category}_{instance}_{elevation}_{azimuth}_{lightening}.npy'
-    data = np.load(file_path, allow_pickle=True)
-    return data
+files = os.listdir(f'./tmp')
 
-# List of categories to process
-categories = ['animal', 'human', 'airplane', 'car', 'truck']
-
-# Initialize dictionaries to store data arrays by category
-data_arrays = {category: np.array([]) for category in categories}
-
-# Iterate through each condition
 for elevation in [0, 2, 4, 6, 8]:
     for azimuth in [0, 8, 16, 24, 32]:
         for lightening in [0, 1, 2, 3, 4]:
-            # Reset arrays for each condition
-            for category in categories:
-                data_arrays[category] = np.array([])
-            
+            animal = np.array([])
+            human = np.array([])
+            airplane = np.array([])
+            car = np.array([])
+            truck = np.array([])
             for instance in [4, 6, 7, 8, 9]:
-                for category in categories:
-                    if data_arrays[category].size == 0:
-                        data_arrays[category] = load_and_append_data(category, instance, elevation, azimuth, lightening)
-                    else:
-                        new_data = load_and_append_data(category, instance, elevation, azimuth, lightening)
-                        data_arrays[category] = np.append(data_arrays[category], new_data, axis=0)
+                if len(animal) == 0:
+                    animal = np.load(f'./tmp/animal_{instance}_{elevation}_{azimuth}_{lightening}.npy', allow_pickle = True)
+                else:
+                    animal = np.append(animal, np.load(f'./tmp/animal_{instance}_{elevation}_{azimuth}_{lightening}.npy', allow_pickle = True), axis = 0)
+                    
+                if len(human) == 0:
+                    human = np.load(f'./tmp/human_{instance}_{elevation}_{azimuth}_{lightening}.npy', allow_pickle = True)
+                else:
+                    human = np.append(human, np.load(f'./tmp/human_{instance}_{elevation}_{azimuth}_{lightening}.npy', allow_pickle = True), axis = 0)
+        
+                if len(human) == 0:
+                    airplane = np.load(f'./tmp/airplane_{instance}_{elevation}_{azimuth}_{lightening}.npy', allow_pickle = True)
+                else:
+                    airplane = np.append(airplane, np.load(f'./tmp/airplane_{instance}_{elevation}_{azimuth}_{lightening}.npy', allow_pickle = True), axis = 0)
+        
+                if len(car) == 0:
+                    car = np.load(f'./tmp/car_{instance}_{elevation}_{azimuth}_{lightening}.npy', allow_pickle = True)
+                else:
+                    car = np.append(car, np.load(f'./tmp/car_{instance}_{elevation}_{azimuth}_{lightening}.npy', allow_pickle = True ), axis = 0)
+        
+                if len(truck) == 0:
+                    truck = np.load(f'./tmp/truck_{instance}_{elevation}_{azimuth}_{lightening}.npy', allow_pickle = True)
+                else:
+                    truck = np.append(truck, np.load(f'./tmp/truck_{instance}_{elevation}_{azimuth}_{lightening}.npy', allow_pickle = True), axis = 0)
 
-            # Save aggregated data for each category and condition
-            for category in categories:
-                np.save(f"./smallnorb_split/{category}_{elevation}_{azimuth}_{lightening}.npy", data_arrays[category])
+            np.random.shuffle(animal)
+            np.random.shuffle(human)
+            np.random.shuffle(airplane)
+            np.random.shuffle(car)
+            np.random.shuffle(truck)
 
+            np.save(f"./smallnorb_split/animal_{elevation}_{azimuth}_{lightening}.npy", animal)
+            np.save(f"./smallnorb_split/human_{elevation}_{azimuth}_{lightening}.npy", human)
+            np.save(f"./smallnorb_split/airplane_{elevation}_{azimuth}_{lightening}.npy", airplane)
+            np.save(f"./smallnorb_split/truck_{elevation}_{azimuth}_{lightening}.npy", truck)
+            np.save(f"./smallnorb_split/car_{elevation}_{azimuth}_{lightening}.npy", car)
